be9445b8288ef7ae7d1adc52423264b1e992e2c5
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-07-15 09:29:42 +0200

    Update assembly info

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RadiantMapToWavefrontObj")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Unattended")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RadiantMapToWavefrontObj")]
-[assembly: AssemblyCopyright("Copyright © Unattended 2017")]
+[assembly: AssemblyCopyright("Copyright © Wesley Baartman 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
