03d1b86aa6c78e27e52bf3caaea531e2142aa104
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-07-16 14:44:03 +0200

    Add Edge class

---
+++ src/Edge.cs
@@ -0,0 +1,16 @@
+﻿
+namespace RadiantMapToWavefrontObj
+{
+    public struct Edge
+    {
+        public Vertex A { get; set; }
+        public Vertex B { get; set; }
+
+        // Constructor for an edge between two vertices.
+        public Edge(Vertex a, Vertex b)
+        {
+            A = a;
+            B = b;
+        }
+    }
+}
