892fb241ae9ccf48a58541ddb61519cb58f20234
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2017-07-21 12:03:43 +0200

    Add patchDef2 support

--- src/Patch.cs
+++ src/Patch.cs
@@ -64,30 +64,37 @@ namespace RadiantMapToWavefrontObj
         // Creates a radiant patch from a piece of code.
         public static Patch CreateFromCode(string[] code)
         {
-            string sizePattern = @"(\s+)?\(\s?(\d+)\s(\d+)\s(\d+)\s(\d+)\s(\d+)\s(\d+)\s(\d+)\s?\)";    //width,height [2,3]
+            string sizePattern = @"(\s+)?\(\s?(\d+)\s(\d+)\s(\d+)\s(\d+)\s(\d+)(\s(\d+)\s(\d+))?\s?\)";    //width,height [2,3]
             Regex sizeRegex = new Regex(sizePattern, RegexOptions.IgnoreCase);
 
             Patch patch = null;
 
-            foreach (string line in code)
+            int line = 0;
+
+            while (line < code.Length)
             {
-                Match m = sizeRegex.Match(line);
+                Match m = sizeRegex.Match(code[line]);
+                ++line;
                 if (m.Success)
+                {
                     patch = new Patch(int.Parse(m.Groups[2].ToString()), int.Parse(m.Groups[3].ToString()));
+                    break;
+                }
             }
 
             string num = @"-?\d+(\.\d+)?";
             string vertex = @"(\(\s?(" + num + @")\s(" + num + @")\s(" + num + @")\s("      //x,y,z [2,4,6]
-                + num + @")\s(" + num + @")\s(" + num + @")\s(" + num + @")\s("
-                + num + @")\s(" + num + @")\s(" + num + @")\s?\))";
+                + num + @")\s(" + num + @")\s("                                             //patchDef2
+                + "(" + num + @")\s(" + num + @")\s(" + num + @")\s(" + num + @")\s(" + num + @"))?\s?\))"; //patchDef3
 
             string pattern = vertex;
 
             Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
 
-            foreach (string line in code)
+            while (line < code.Length)
             {
-                MatchCollection m = regex.Matches(line);
+                MatchCollection m = regex.Matches(code[line]);
+                ++line;
                 if (m.Count > 0)
                 {
                     for (int i = 0; i < m.Count; ++i)
