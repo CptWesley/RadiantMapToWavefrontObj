6834ab2522d10e0a34fb20bc0d0c4f8041332aa1
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2021-01-06 21:06:29 +0100

    Add texture size search support

--- src/RadiantMapToObj/Internal/TextureLoading/TextureFinder.cs
+++ src/RadiantMapToObj/Internal/TextureLoading/TextureFinder.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using RadiantMapToObj.Configuration;
+using SixLabors.ImageSharp;
 
 namespace RadiantMapToObj.Internal.TextureLoading
 {
@@ -11,13 +13,48 @@ namespace RadiantMapToObj.Internal.TextureLoading
     internal static class TextureFinder
     {
         /// <summary>
-        /// Finds the specified settings.
+        /// Finds the specified texture with the given search settings.
         /// </summary>
         /// <param name="settings">The settings for the texture search.</param>
         /// <param name="texture">The texture name.</param>
         /// <returns>The data stream of the texture.</returns>
         public static Stream? Find(TextureSettings settings, string texture)
             => FindInDirectory(settings, texture.Split('/', '\\'), new List<string>());
+
+        /// <summary>
+        /// Finds the size of the texture with the given search settings.
+        /// </summary>
+        /// <param name="settings">The settings for the texture search.</param>
+        /// <param name="texture">The texture.</param>
+        /// <returns>The size of the image or (0, 0) if the image couldn't be loaded.</returns>
+        [SuppressMessage("Microsoft.Design", "CA1031", Justification = "Any exception should be dealt with.")]
+        public static (int Width, int Height) FindSize(TextureSettings settings, string texture)
+        {
+            using Stream? stream = Find(settings, texture);
+
+            if (stream is null)
+            {
+                return (0, 0);
+            }
+
+            try
+            {
+                using var image = Image.Load(stream);
+                return (image.Width, image.Height);
+            }
+            catch
+            {
+                try
+                {
+                    using var tga = Pfim.Pfim.FromStream(stream);
+                    return (tga.Width, tga.Height);
+                }
+                catch
+                {
+                    return (0, 0);
+                }
+            }
+        }
 
         private static Stream? FindInDirectory(TextureSettings settings, string[] texture, List<string> path)
         {
